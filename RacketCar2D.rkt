c650fe8d2bcff0b3b4ad92afd0988a654e018730
Ben Thompson <ben.n.thompson@gmail.com>
2016-12-30 17:47:48 -0500

    Nothing much

--- RacketCar2D.rkt
+++ RacketCar2D.rkt
@@ -4,8 +4,8 @@
   racket/draw)
 
 (struct color (red green blue alpha) #:transparent)
+(struct car_wheel (size power color) #:transparent)
 (struct car_segment (angle1 length1 angle2 length2 color wheel) #:transparent)
-(struct car_body (angle1 angle2 angle3 angle4 angle5 angle6 angle7 angle8) #:transparent)
-(struct car_wheel (size power) #:transparent)
-(struct car_wheels (wheel1 wheel2 wheel3 wheel4) #:transparent)
+(struct car_body (segment1 segment2 segment3 segment4) #:transparent)
+(struct phys_info () #:transparent)
 (struct car (body wheels phys) #:transparent)
